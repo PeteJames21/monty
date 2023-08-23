7fdfca7cd3d5817b5086c8e9759799fe1cca9b65
PeteJames21 <peetjames21@gmail.com>
2023-08-24 01:59:30 +0300

    Rename header file

---
+++ monty.h
@@ -0,0 +1,50 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern stack_t *main_stack;
+
+/* exec.c */
+int exec(char *opcode, char *arg, unsigned int line_no, stack_t **stack);
+void (*get_op_func(char *opcode)) (stack_t **stack, unsigned int line_number);
+
+/* ops.c */
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, int n);
+
+/* stack.c */
+void free_stack(stack_t *top);
+
+/* utils.c */
+int is_int(char *s);
+
+#endif
